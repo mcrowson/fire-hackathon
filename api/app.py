b1f3fe14f50f023e8569975c6715baf794a67f04
Matthew Crowson <matthew.d.crowson@gmail.com>
2015-06-06 12:58:38 -0400

    initial API app and db

---
+++ api/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask.ext.restless import APIManager
+from flask.ext.sqlalchemy import SQLAlchemy
+ 
+app = Flask(__name__)
+ 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///firefighters.db'
+db = SQLAlchemy(app)
+ 
+class Firefighter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text)
+    last_name = db.Column(db.Text)
+
+class MeasurementObject(db.Model):
+    pass
+
+class Sensor(db.Model):
+    pass
+
+db.create_all()
+
+ 
+api_manager = APIManager(app, flask_sqlalchemy_db=db)
+api_manager.create_api(Firefighter, methods=['GET', 'POST', 'DELETE', 'PUT'])
+ 
+if __name__ == "__main__":
+    app.run()
