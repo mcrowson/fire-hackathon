2eeacb3c9864307976b57b2384f86718d51f64cf
A_Patton <antoinepatton724@gmail.com>
2015-06-06 12:52:46 -0400

    test push

--- PycharmProjects/firehack/app.py
+++ PycharmProjects/firehack/app.py
@@ -1,1 +1,13 @@
-__author__ = 'Rico'
+__author__ = 'antoinepatton'
+
+import sqlite3
+from flask import Flask, request, session
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+
+
+if __name__ == '__main__':
+    app.run()
