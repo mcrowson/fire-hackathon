2deb26364366f2e16ba40e5f54ee5929cd409f81
A_Patton <antoinepatton724@gmail.com>
2015-06-18 16:55:26 -0400

    getting fields on the first add website page

--- PycharmProjects/firehack/app.py
+++ PycharmProjects/firehack/app.py
@@ -1,12 +1,18 @@
 __author__ = 'antoinepatton'
 
 import sqlite3
-from flask import Flask, request, session
+from flask import Flask, request, session, render_template
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+@app.route('/')
+def get_initial_data():
+    #events = api.call(get_event, arg0, arg1)
+    data = dict()
+    data['test'] = "whew who"
 
+    return render_template('index.html', data)
 
 
 if __name__ == '__main__':
